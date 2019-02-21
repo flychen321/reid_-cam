8d786f5135f7b8525d2911ea1cdf4bd4288de676
dl <571435345@qq.com>
2019-02-21 12:34:23 +0800

    add change index for train_all

--- changeIndex.py
+++ changeIndex.py
@@ -4,35 +4,48 @@ import numpy as np
 import os
 from shutil import copyfile
 
-original_path='./data/market/pytorch'
+original_path = './data/market/pytorch'
 
-#copy folder tree from source to destination
-def copyfolder(src,dst):
-    files=os.listdir(src)
+
+# copy folder tree from source to destination
+def copyfolder(src, dst):
+    files = os.listdir(src)
     if not os.path.isdir(dst):
         os.mkdir(dst)
-    for tt in files:   
-        copyfile(src+'/'+tt,dst+'/'+tt)
+    for tt in files:
+        copyfile(src + '/' + tt, dst + '/' + tt)
+
 
 train_save_path = original_path + '/val_new'
-data_path=original_path+'/val'
+data_path = original_path + '/val'
 if not os.path.isdir(train_save_path):
     os.mkdir(train_save_path)
 
-reid_index=0
-folders=os.listdir(data_path)
+reid_index = 0
+folders = os.listdir(data_path)
 for foldernames in folders:
-    copyfolder(data_path+'/'+foldernames,train_save_path+'/'+str(reid_index).zfill(4))
-    reid_index=reid_index+1
-
+    copyfolder(data_path + '/' + foldernames, train_save_path + '/' + str(reid_index).zfill(4))
+    reid_index = reid_index + 1
 
 train_save_path = original_path + '/train_new'
-data_path=original_path+'/train'
+data_path = original_path + '/train'
 if not os.path.isdir(train_save_path):
     os.mkdir(train_save_path)
 
-reid_index=0
-folders=os.listdir(data_path)
+reid_index = 0
+folders = os.listdir(data_path)
 for foldernames in folders:
-    copyfolder(data_path+'/'+foldernames,train_save_path+'/'+str(reid_index).zfill(4))
-    reid_index=reid_index+1
+    copyfolder(data_path + '/' + foldernames, train_save_path + '/' + str(reid_index).zfill(4))
+    reid_index = reid_index + 1
+
+train_save_path = original_path + '/train_all_new'
+data_path = original_path + '/train_all'
+if not os.path.isdir(train_save_path):
+    os.mkdir(train_save_path)
+
+reid_index = 0
+folders = os.listdir(data_path)
+for foldernames in folders:
+    copyfolder(data_path + '/' + foldernames, train_save_path + '/' + str(reid_index).zfill(4))
+    reid_index = reid_index + 1
+
