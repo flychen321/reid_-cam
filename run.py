243b33017443e87a10ec078134608373d97e4b6a
dl <571435345@qq.com>
2019-02-26 12:00:52 +0800

    temp commit

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 value = [[0, 100], [70, 90], [60, 80], [60, 90], [50, 100], [80, 90], [70, 80], [80, 80], [90, 90], ]
-for i in np.arange(5):
+for i in np.arange(1):
     print('i = %.3f' % i)
     log_name = 'log/' + 'log_' + str(i)
     print('log name = %s' % log_name)
@@ -11,15 +11,19 @@ for i in np.arange(5):
     print('cmd = %s' % cmd)
     os.system(cmd)
 
-    os.system('python test.py  --use_dense  --ratio ' + str(0) + ' >>  ' + log_name)
+    os.system('python test.py  --use_dense  --mode ' + str(0) + ' >>  ' + log_name)
     os.system('python evaluate.py' + ' >> ' + log_name)
     os.system('python evaluate_rerank.py' + ' >> ' + log_name)
 
-    os.system('python test.py  --use_dense  --ratio ' + str(1) + ' >>  ' + log_name)
+    os.system('python test.py  --use_dense  --mode ' + str(1) + ' >>  ' + log_name)
     os.system('python evaluate.py' + ' >> ' + log_name)
     os.system('python evaluate_rerank.py' + ' >> ' + log_name)
 
-    os.system('python test.py  --use_dense  --ratio ' + str(2) + ' >>  ' + log_name)
+    os.system('python test.py  --use_dense  --mode ' + str(2) + ' >>  ' + log_name)
+    os.system('python evaluate.py' + ' >> ' + log_name)
+    os.system('python evaluate_rerank.py' + ' >> ' + log_name)
+
+    os.system('python test.py  --use_dense  --mode ' + str(3) + ' >>  ' + log_name)
     os.system('python evaluate.py' + ' >> ' + log_name)
     os.system('python evaluate_rerank.py' + ' >> ' + log_name)
 
